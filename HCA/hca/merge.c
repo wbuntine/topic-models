1e301bc21daf3dda084fc45f6cd6f1d52e772559
Wray Buntine <wbuntine@users.noreply.github.com>
2014-07-26 08:39:48 +1000

    working on it

--- HCA/hca/merge.c
+++ HCA/hca/merge.c
@@ -33,7 +33,7 @@
  *    adapted by diffing code in like.c
  */ 
 
-double likemerge_DIRalpha(k1,k2) {
+static double likemerge_DIRalpha(k1,k2) {
   /*
    *   Dirichlet for topics
    */
@@ -51,7 +51,7 @@ double likemerge_DIRalpha(k1,k2) {
   return likelihood;
 }
 
-double likemerge_DIRbeta(k1,k2) {
+static double likemerge_DIRbeta(k1,k2) {
   int j,t;
   double likelihood = 0;
   double val = 0;
@@ -71,12 +71,19 @@ double likemerge_DIRbeta(k1,k2) {
   return likelihood;
 }
 
-double alphabasechange(int t) {
-  if ( ddP.PYalpha==H_HDP )
-    return (TDt + ddP.b0*ddP.alphapr[t]);
-  if ( ddP.PYalpha==H_PDP )
+static double merge_alphabasetopicprob(uint32_t TDT, uint32_t TDt, int t) {
+  assert(t>=0);
+  if ( ddP.PYalpha==H_PDP ) 
     return ddP.alphapr[t];
-  return (TDt - ddP.a0)/(TDT + ddP.b0)
+  if ( ddP.PYalpha==H_HDP ) {
+    assert(ddP.a0==0);
+    return  ( (double)TDt+ddP.b0*ddP.alphapr[t] )
+      /((double)TDT+ddP.b0);
+  }
+  /*
+   *   normal situation when empty topics exist
+   */
+  return  ((double)TDt-ddP.a0)/((double)TDT+ddP.b0);	 
 }
 
 /*
@@ -84,75 +91,98 @@ double alphabasechange(int t) {
  */
 double likemerge(int k1, int k2) {
   int t, d, k;
+
   /*
-   *   storing/saving
+   *   storing/saving the new/proposed version
    */
   uint32_t TDt;
   uint16_t *Tdt;
   /*
-   *  cache
+   *  cache for doc side change
    */
   uint16_t *TdT;
   uint16_t *Ndt;
+
+  int sortD;
   /*
-   *    sorting on moves
+   *    sorting on moves,
+   *    stores doc index for 
    */
-  uint16_t *Tdt_up;
-  uint16_t *Tdt_down;
+  uint32_t *Tdt_up;
+  uint32_t *Tdt_down;
+  /*
+   *     change from incr/decr this docs Tdt
+   */
   float *score_up;
   float *score_down;
+  /*
+   *     change from incr/decr total TDt
+   */
   float base_up;
   float base_down;
 
   double likelihood = 0;
 
-  if ( k1<=0 || k2<=0 ) 
+  if ( k1<=0 || k2<=0 || ddS.TDt[k1]==0 || ddS.TDt[k2]==0 ) 
     return 0.0;
  
   /*
-   *    we save all the old values relevant to topic k1,
-   *    and overwrite temporarily so we can use standard
-   *    routines/scoring functions
+   *   build local store
    */
   Tdt = u16vec(ddN.DT); 
-  TdT = u16vec(ddN.DT);
+  TdT = u16vec(ddN.DT); 
   Ndt = u16vec(ddN.DT);
-  Tdt_up = u16vec(ddN.DT);
-  Tdt_down = u16vec(ddN.DT);
-  score_up = u16vec(ddN.DT);
-  score_down = u16vec(ddN.DT);
+  Tdt_up = u32vec(ddN.DT);
+  Tdt_down = u32vec(ddN.DT);
+  score_up = fvec(ddN.DT);
+  score_down = fvec(ddN.DT);
   if ( !score_down || !score_up || !Ndt || !TdT ||
        !Tdt || !Tdt_up || !Tdt_down )
     yap_quit("Out of memory in likemerge()\n");
 
   /*
-   *  initialise
+   *  initialise all data entries to simple sum
    */
-  TDt = 0; ???????????????
+  TDt = 0;
   for (d=0; d<ddN.DT; d++) {
-    Tdt[d] = ddS.Tdt[d][k1];
+    Tdt[d] = ddS.Tdt[d][k1] + ddS.Tdt[d][k2];
     Ndt[d] = ddS.Ndt[d][k1] + ddS.Ndt[d][k2];
-    TdT[d] = 0;  
+    TdT[d] = 0;
     for (k=0; k<ddN.T; k++)
       TdT[d] += ddS.Tdt[d][k];
-    ddS.Tdt[d][k1] += ddS.Tdt[d][k2];
-    if ( ddS.Tdt[d][k1]>2 )
-      ddS.Tdt[d][k1]--;
-    TDt += ddS.Tdt[d][k1];
-  }
-  for (d=0; d<ddN.DT; d++) {
-    Tdt_up[d] = d;
-    Tdt_down[d] = d;
-    score_up[d] = (ddP.bpar + ddP.apar*TdT[d])
-      * S_V(ddP.SX,Ndt[d],Tdt[d]+1) * alphabasetopicprob*(t);
-    score_down[d] = 1.0/(ddP.bpar + ddP.apar*(TdT[d]-1))
-      / S_V(ddP.SX,Ndt[d],Tdt[d]) / alphabasetopicprob*(t-1);
+    TDt += Tdt[d];
   }
 
+  /*
+   *  initialise sort
+   */
+  sortD = 0;
+  for (d=0; d<ddN.DT; d++) {
+    /*   don't change for some docs */
+    if ( Ndt[d]<=1 )
+      continue;
+    Tdt_up[sortD] = d;
+    Tdt_down[sortD] = d;
+    sortD++;
+    if ( Tdt[d]<Ndt[d] )
+      score_up[d] = (ddP.bpar + ddP.apar*TdT[d]) / S_V(ddP.SX,Ndt[d],Tdt[d]+1) 
+	* merge_alphabasetopicprob(TDT, TDt, t);
+    else 
+      score_up[d] = 0;
+    if ( Tdt[d]>1 )
+      score_down[d] = S_V(ddP.SX,Ndt[d],Tdt[d])/(ddP.bpar + ddP.apar*(TdT[d]-1))
+	/ merge_alphabasetopicprob(TDT-1, TDt-1, t);
+    else
+      score_down[d] = 0;
+  }
+  /*  sortD is count of docs to be optimised */
+  /*  use a heap, so only top of heap is least */
+
+  free(TdT);
+  free(Ndt);
+  free(Tdt);
   free(score_up);
   free(score_down);
-  free(TdT);
-  free(Tdt);
   free(Tdt_up);
   free(Tdt_down);
   yap_infinite(likelihood);
